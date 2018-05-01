580a82879ce316e1c35f9327cfc07ca22871a7bb
zjarhd <zjarhd171@gmail.com>
2018-05-01 17:26:38 +0900

    update mul

---
+++ src/main/java/com/zjarhd/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.zjarhd.calculator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+	int sum(int a, int b) {
+		return a+b;
+	}
+	int mul(int a, int b) {
+		return a*b;
+	}
+}
